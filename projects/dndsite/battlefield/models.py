1c419e228622e80672ed37d76258350f917878a7
rom341 <roman.murzik.antpnov@gmail.com>
2025-10-31 19:51:17 +0400

    added registration / login logic

--- projects/dndsite/battlefield/models.py
+++ projects/dndsite/battlefield/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.username
+    
 class Group(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -8,11 +14,12 @@ class Group(models.Model):
         return self.name
     
 class Character(models.Model):
+    user = models.ForeignKey(User, related_name='characters', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     group = models.ForeignKey(Group, related_name='characters', on_delete=models.CASCADE)
-    hit_points = models.IntegerField()
-    armor_class = models.IntegerField()
-    initiative = models.IntegerField()
+    hit_points = models.IntegerField(default=0)
+    armor_class = models.IntegerField(default=0)
+    initiative = models.IntegerField(default=0)
     position_x = models.IntegerField(default=0)
     position_y = models.IntegerField(default=0)
 
--- projects/dndsite/battlefield/views.py
+++ projects/dndsite/battlefield/views.py
@@ -1,18 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from battlefield.models import Group
 from battlefield.forms.move_character_form import MoveCharacterForm
 
 # Create your views here.
-def battle(request):
+def battle(request):    
+    print("Battle View Accessed")
     group = get_object_or_404(Group, pk=1)
     characters = group.characters.all()
+    
+    form = MoveCharacterForm()
+    if request.method == 'POST':
+        form = MoveCharacterForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.user = request.user
+            message.group = group
+            
+            message.save()
+            print(f"Character moved to ({message.position_x}, {message.position_y})")
+            return redirect('battle')
+        
     data = {
-        'extra_text': 'Roman',
         'rows_range': range(10),
         'cols_range': range(5),
         'characters': characters,
+        'form': form,
     }
     
-    form = MoveCharacterForm()
     return render(request, 'battlefield.html', data)
