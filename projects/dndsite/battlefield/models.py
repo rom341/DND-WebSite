af29d50b80a4dedaf54adbd0548f1765f2e18c2f
ILUCHAFURGON <ibeloborodov13@gmail.com>
2025-11-01 17:23:05 +0100

    добавлены статы для персонажа

--- projects/dndsite/battlefield/models.py
+++ projects/dndsite/battlefield/models.py
@@ -8,15 +8,31 @@ class Group(models.Model):
     def __str__(self):
         return self.name
     
+class CharacterStats(models.Model):
+    strength = models.IntegerField(default=0)
+    """Main character stat СИЛА"""
+    dexterity = models.IntegerField(default=0)
+    """Main character stat ЛОВКОСТЬ"""
+    constitution = models.IntegerField(default=0)
+    """Main character stat ТЕЛОСЛОЖЕНИЕ"""
+    intelligence = models.IntegerField(default=0)
+    """Main character stat ИНТЕЛЛЕКТ"""
+    wisdom = models.IntegerField(default=0)
+    """Main character stat МУДРОСТЬ"""
+    charisma = models.IntegerField(default=0)
+    """Main character stat ХАРИЗМА"""
+
 class Character(models.Model):
     user = models.ForeignKey(User, related_name='characters', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    group = models.ForeignKey(Group, related_name='characters', on_delete=models.CASCADE)
-    hit_points = models.IntegerField(default=0)
+    group = models.ForeignKey(Group, related_name='characters', on_delete=models.CASCADE, null=True, blank=True)
+    stats = models.ForeignKey(CharacterStats, related_name='character', on_delete=models.CASCADE, null=True, blank=True)
+    max_hit_points = models.IntegerField(default=0)
+    current_hit_points = models.IntegerField(default=0)
     armor_class = models.IntegerField(default=0)
-    initiative = models.IntegerField(default=0)
     position_x = models.IntegerField(default=0)
     position_y = models.IntegerField(default=0)
+    movement_speed = models.IntegerField(default=0)
 
     class Meta:
         constraints = [
@@ -27,4 +43,5 @@ class Character(models.Model):
         ]
 
     def __str__(self):
-        return f"ID{self.id}: {self.name} (HP: {self.hit_points}, AC: {self.armor_class}, Init: {self.initiative}, Pos: ({self.position_x}, {self.position_y}))"
+        return f"ID{self.id}: {self.name} (HP: {self.max_hit_points}, AC: {self.armor_class}, Pos: ({self.position_x}, {self.position_y}))"
+    
