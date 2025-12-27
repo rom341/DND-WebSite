df36d9038de48659e3b455e0213998ac9815f5be
Roman <roman.murzik.antpnov@gmail.com>
2025-12-27 23:53:07 +0400

    all template inputs moved to one group, fixed template input filling when template changed, fixed character creation

--- projects/dndsite/static/js/create_character_template_select.js
+++ projects/dndsite/static/js/create_character_template_select.js
@@ -1,18 +1,3 @@
-// document.getElementById('selected_character_base').addEventListener('change', function() {
-//     const selectedKey = this.value;
-//     const nameField = document.getElementById('character_name');
-//     const raceField = document.getElementById('race');
-
-//     if (selectedKey && templates[selectedKey]) {
-//         nameField.value = templates[selectedKey].subject;
-//         raceField.value = templates[selectedKey].body;
-//     } else {
-//         nameField.value = '';
-//         raceField.value = '';
-//     }
-// });
-
-// Ждем полной загрузки DOM
 document.addEventListener('DOMContentLoaded', function() {
     const dataSource = document.getElementById('templates-json');
     if (!dataSource) return;
@@ -23,30 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
     selector.addEventListener('change', function() {
         const selectedId = this.value;
         const data = templatesData[selectedId];
-
+        console.log(data)
         if (data) {
-            // Словарь соответствий: "ID в HTML": "Ключ в словаре Python"
-            const mapping = {
-                'character_name': 'character_name',
-                'race': 'race',
-                'class': 'character_class',        // Исправлено под ваш словарь
-                'subclass': 'character_sub_class', // Исправлено под ваш словарь
-                'strength': 'strength',
-                'hit_points': 'max_hit_points',    // Исправлено под ваш словарь
-                'armor_class': 'armor_class',
-                'movement_speed': 'movement_speed',
-                'aligment': 'alignment',
-                'size': 'size',
-                'age': 'age',
-                'height': 'height',
-                'weight': 'weight'
-            };
-
-            for (let [htmlId, apiKey] of Object.entries(mapping)) {
+            for (let [htmlId, apiKey] of Object.entries(data)) {
                 const input = document.getElementById(htmlId);
+                console.log(input)
                 if (input) {
-                    // Если значение null или undefined, ставим пустую строку/0
-                    let val = data[apiKey];
+                    let val = apiKey;
+                    console.log(val)
                     input.value = (val !== null && val !== undefined) ? val : (input.type === 'number' ? 0 : "");
                 }
             }
