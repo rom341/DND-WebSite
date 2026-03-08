1969b4e8f0635e7d1a82c7f97ad3822beb0c2469
Roman <roman.murzik.antpnov@gmail.com>
2026-03-08 20:59:46 +0400

    removed commented code

--- projects/dndsite/battlefield/utils/contexts/character_position_context.py
+++ projects/dndsite/battlefield/utils/contexts/character_position_context.py
@@ -15,25 +15,3 @@ class CharacterPositionContextContainer:
     
     def get_context(self):
         return self.to_dict()
-
-# class CharacterPositionContextContainer:
-#     """Class to encapsulate character position context data"""
-#     def __init__(self, character_positions=None):
-#         self.character_positions = character_positions if character_positions is not None else []
-
-#     def get_context(self) -> dict:
-#         """Return context dictionary for character positions"""
-#         #return self.__dict__.copy()
-#         positions_list = []
-#         for p in self.character_positions:
-#             positions_list.append({
-#                 "id": p.character.id,
-#                 "name": p.character.character_name,
-#                 "row": p.row,
-#                 "col": p.column,
-#             })
-        
-#         return {
-#             "character_positions": self.character_positions, 
-#             "character_positions_dict": positions_list         
-#         }
