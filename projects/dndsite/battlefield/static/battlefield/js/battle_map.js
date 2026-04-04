dc5d598195d9494155f5198d4615c188150a6c0b
Roman <roman.murzik.antpnov@gmail.com>
2026-04-04 22:25:00 +0400

    added support for multiple characters visualisation in one battle map cell

--- projects/dndsite/battlefield/static/battlefield/js/battle_map.js
+++ projects/dndsite/battlefield/static/battlefield/js/battle_map.js
@@ -4,30 +4,36 @@ export async function renderBattleMap() {
     const battleState = await stateReady;
     const gridMap = document.getElementById("battle-map-grid");
     const cellTemplate = document.getElementById("battle-map-grid-cell-template");
+    const characterMarkTemplate = document.getElementById("battle-map-character-mark-template");
     
     gridMap.style.display = 'grid';
     gridMap.style.width = '100%';
     gridMap.style.gridTemplateColumns = `repeat(${battleState.locationData.columns_count}, 50px)`;
     gridMap.innerHTML = '';
+    console.log(battleState);
     for (let r = 0; r < battleState.locationData.rows_count; r++) {
         for (let c = 0; c < battleState.locationData.columns_count; c++) {            
-            const fragment = cellTemplate.content.cloneNode(true);
-            const cellInstance = fragment.querySelector('.battle-map-grid-cell');
+            const gridCellFragment = cellTemplate.content.cloneNode(true);
+            const gridCellInstance = gridCellFragment.querySelector('.battle-map-grid-cell');
             
-            cellInstance.className = "grid-cell";
-            cellInstance.dataset.x = c;
-            cellInstance.dataset.y = r;
+            gridCellInstance.className = "grid-cell";
+            gridCellInstance.dataset.x = c;
+            gridCellInstance.dataset.y = r;
 
-            const characterPosition = battleState.characterPositions.find(p => p.column == c && p.row == r);  
+            const characterPositions = battleState.characterPositions.filter(p => p.column == c && p.row == r);
+            if (characterPositions.length > 0) {
+                characterPositions.forEach(characterPosition => {
+                    const characterMarkFragment = characterMarkTemplate.content.cloneNode(true);
+                    const characterMarkInstance = characterMarkFragment.querySelector('.character_mark');
+                    if (characterMarkInstance != null) {
+                        console.log("fd");
+                        characterMarkInstance.textContent = characterPosition.character.character_name;
+                        gridCellInstance.appendChild(characterMarkInstance);
+                    }           
+                });
 
-            if (characterPosition) {
-                const mark = cellInstance.querySelector('.character_mark');
-                mark.textContent = characterPosition.character.character_name;
-            } else {
-                cellInstance.querySelector('.character_mark').remove();
             }
-
-            gridMap.appendChild(cellInstance);
+            gridMap.appendChild(gridCellInstance);
         }
     }
 }
