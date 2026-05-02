415ef9ea2e33453c3e983a71d033d988270b766d
rom341 <roman.murzik.antpnov@gmail.com>
2026-05-02 15:28:15 +0300

    improved zoom and added pan

--- projects/dndsite/location/static/location/js/battle/viewport.js
+++ projects/dndsite/location/static/location/js/battle/viewport.js
@@ -1,4 +1,5 @@
 import { EventObserver } from "../services/event_observer.js";
+import { Vector2, Vector2Math } from "../services/math.js";
 
 export class Viewport {
     constructor (game, canvas, background) {
@@ -7,10 +8,21 @@ export class Viewport {
         this.context = this.canvas.getContext("2d");
         this.onDrawObserver = new EventObserver();
 
-        this.zoom = 1;
-        this.zoomSensivity = 0.1;
-        this.zoomMax = 2;
-        this.zoomMin = 0.2;
+        this.zoom = {
+            value: 1,
+            sensivity: 0.01,
+            max: 2,
+            min: 0.2
+        }
+        this.offset = new Vector2();
+
+        this.drag = {
+            startPosition: new Vector2(),
+            endPosition: new Vector2(),
+            offset: new Vector2(),
+            active: false
+        }
+
         this.initEvents();
     }
 
@@ -21,20 +33,81 @@ export class Viewport {
     draw() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.save();
-        this.context.scale(this.zoom, this.zoom);
+        this.scale_context();
+        this.translate_context();
         this.onDrawObserver.broadcast(this.context);
         this.context.restore();
+    }
+    
+    scale_context() {
+        this.context.scale(this.zoom.value, this.zoom.value);
+    }
+
+    translate_context() {
+        const current_offset = this.getOffset();
+        this.context.translate(current_offset.x, current_offset.y);
     }
 
     initEvents() {
         this.canvas.addEventListener("wheel", this.onMouseWheelScroll.bind(this), { passive: false });
+        this.canvas.addEventListener("mousedown", this.onMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.onMouseMove.bind(this));
+        this.canvas.addEventListener("mouseup", this.onMouseUp.bind(this));
     }
 
     onMouseWheelScroll(e) {
         e.preventDefault();
+        const mouseBefore = this.getMousePosition(e);
+
         const direction = Math.sign(e.deltaY);
-        this.zoom += direction * this.zoomSensivity;
-        this.zoom = Math.max(this.zoomMin, Math.min(this.zoom, this.zoomMax));
+        const oldZoom = this.zoom.value;
+        this.zoom.value -= direction * this.zoom.sensivity;
+        this.zoom.value = Math.max(this.zoom.min, Math.min(this.zoom.value, this.zoom.max));
+
+        const mouseAfter = this.getMousePosition(e);
+
+        const diff = Vector2Math.subtract(mouseAfter, mouseBefore);
+        this.offset = Vector2Math.add(this.offset, diff);
+
         this.draw();
+    }
+
+    onMouseDown(e) {
+        if (e.button == 0) { //mouse wheel click
+            this.drag.startPosition = this.getMousePosition(e);
+            this.drag.active = true;
+        }
+    }
+
+    onMouseMove(e) {
+        if (this.drag.active) {
+            this.drag.endPosition = this.getMousePosition(e);
+            this.drag.offset = Vector2Math.subtract(this.drag.endPosition, this.drag.startPosition);
+            this.draw();
+        }
+    }
+
+    onMouseUp(e) {
+        if (this.drag.active) {
+            this.offset = Vector2Math.add(this.offset, this.drag.offset);
+            this.drag = {
+                startPosition: new Vector2(),
+                endPosition: new Vector2(),
+                offset: new Vector2(),
+                active: false
+            }
+            this.draw()
+        }
+    }
+
+    getMousePosition(e) {
+        return new Vector2(
+            (e.offsetX / this.zoom.value) - this.offset.x,
+            (e.offsetY / this.zoom.value) - this.offset.y
+        );
+    }
+
+    getOffset() {
+        return Vector2Math.add(this.offset, this.drag.offset);
     }
 }
--- projects/dndsite/location/static/location/js/services/math.js
+++ projects/dndsite/location/static/location/js/services/math.js
@@ -5,3 +5,24 @@ export class GridMath {
         return includeLabels ? [col-1, row-1] : [col, row];
     }
 }
+
+export class Vector2 {
+    constructor (x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export class Vector2Math {
+    static subtract(vector1, vector2) {
+        return new Vector2(vector1.x - vector2.x, vector1.y - vector2.y);
+    }
+
+    static add(vector1, vector2) {
+        return new Vector2(vector1.x + vector2.x, vector1.y + vector2.y);
+    }
+
+    static multiply(vector, multiplier) {
+        return new Vector2(vector.x * multiplier, vector.y * multiplier);
+    }
+}
